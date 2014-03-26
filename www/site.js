06a134344981379ce376eacd5cc47441642680ca
joshbarr <josh@joshbarr.com>
2014-03-27 01:00:51 +1300

    added audio.

--- www/site.js
+++ www/site.js
@@ -1,4 +1,59 @@
+var BufferLoader = (function() {
+    function BufferLoader(context, urlList, callback) {
+      this.context = context;
+      this.urlList = urlList;
+      this.onload = callback;
+      this.bufferList = new Array();
+      this.loadCount = 0;
+    }
+
+    BufferLoader.prototype.loadBuffer = function(url, index) {
+      // Load buffer asynchronously
+      var request = new XMLHttpRequest();
+      request.open("GET", url, true);
+      request.responseType = "arraybuffer";
+
+      var loader = this;
+
+      request.onload = function() {
+        // Asynchronously decode the audio file data in request.response
+        loader.context.decodeAudioData(
+          request.response,
+          function(buffer) {
+            if (!buffer) {
+              alert('error decoding file data: ' + url);
+              return;
+            }
+            loader.bufferList[index] = buffer;
+            if (++loader.loadCount == loader.urlList.length)
+              loader.onload(loader.bufferList);
+          },
+          function(error) {
+            console.error('decodeAudioData error', error);
+          }
+        );
+      }
+
+      request.onerror = function() {
+        alert('BufferLoader: XHR error');
+      }
+
+      request.send();
+    }
+
+    BufferLoader.prototype.load = function() {
+      for (var i = 0; i < this.urlList.length; ++i)
+      this.loadBuffer(this.urlList[i], i);
+    }
+
+    return BufferLoader;
+})();
+
+
 var App = (function(){
+    
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+
 
     var root = document.documentElement;
     var button = document.querySelector("[data-go]");
@@ -13,6 +68,49 @@ var App = (function(){
     var colorText = document.querySelector("[data-color-text]");
     var colorEnd = document.querySelector("[data-color-end]");
     var buttonFs = document.querySelector("[data-fs]");
+
+    var beepStart = document.querySelector("[data-beep-start]");
+    var beepEnd = document.querySelector("[data-beep-end]");
+    var volume = document.querySelector("[data-volume]");
+
+
+
+    var Audio = function() {
+        this.context = new AudioContext();
+    };
+
+    Audio.prototype = {
+        init: function() {
+            var bufferLoader, self = this;
+                bufferLoader = new BufferLoader(
+                this.context,
+                [
+                  'assets/beep.wav'
+                ],
+                function(bufferList) {
+                    self.finishedLoading.call(self, bufferList);
+                }
+                );
+
+            bufferLoader.load();            
+        },
+        finishedLoading: function(bufferList) {
+            this.bufferList = bufferList;
+        },
+        playSound: function(bufferIndex) {
+            var buffer = this.bufferList[bufferIndex];
+            var context = this.context;
+            var gainNode = context.createGainNode();
+            var source = context.createBufferSource(); 
+            source.buffer = buffer;                   
+            source.connect(gainNode);
+            gainNode.connect(context.destination);       
+            gainNode.gain.value = volume.value;
+            source.start(0);
+        }
+    };
+
+
 
 
     var msToTime = function(duration) {
@@ -55,6 +153,17 @@ var App = (function(){
                             root.webkitRequestFullscreen();
                         }
                     }
+                    if (e.target === time) {
+                        if (this.isCounting) {
+                            this.pauseCounter();
+                            return;
+                        }
+                        if (this.currentTime <= 0) {
+                            this.submitForm();
+                            return;
+                        }
+                        this.startCounter();
+                    }
                     break;
                 case "keyup":
                     this.handleKeyPress(e);
@@ -96,10 +205,12 @@ var App = (function(){
         init: function() {
             button.addEventListener("click", this, false);
             buttonFs.addEventListener("click", this, false);
+            time.addEventListener("click", this, false);
             document.documentElement.addEventListener("keyup", this, false);
             document.documentElement.addEventListener("webkitfullscreenchange", this, false);
 
-            
+            this.audio = new Audio();
+            this.audio.init();
 
         },
         submitForm: function() {            
@@ -114,6 +225,10 @@ var App = (function(){
             self.isCounting = true;
             this.updateCounter();
 
+            if (beepStart.checked) {
+                self.audio.playSound(0);
+            }
+
             config.classList.add('hidden');
             fullscreen.classList.remove('hidden');
             time.classList.remove('stop');
@@ -124,6 +239,9 @@ var App = (function(){
             this.interval = setInterval(function() {
                 if (self.currentTime <= 0) {
                     self.stopCounter();
+                    if (beepEnd.checked) {
+                        self.audio.playSound(0);
+                    }
                     return;
                 }
                 self.currentTime -= self.step;
@@ -133,7 +251,7 @@ var App = (function(){
             clearInterval(this.interval);
             this.isCounting = false;
             this.currentTime = 0;
-             if (end_msg.value !== "") {
+            if (end_msg.value !== "") {
                 this.stopMessage = end_msg.value;
             }
         },
@@ -143,7 +261,9 @@ var App = (function(){
                 this.isCounting = false;
                 this.startTime = this.currentTime;
             } else {
-                this.startCounter();
+                if (this.currentTime > 0) {
+                    this.startCounter();
+                }
             }
         },
         updateCounter: function() {
